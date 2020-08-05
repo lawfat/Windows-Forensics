b8a6eb2394a953b03545ba1c94a9983dfc7acb8d
DreamoryIndia <62346630+DreamoryIndia@users.noreply.github.com>
2020-08-05 13:39:22 -0400

    Update prefetch_anlaysis.py

--- prefetch_anlaysis.py
+++ prefetch_anlaysis.py
@@ -10,25 +10,35 @@ def main():
 
     # Get the list of files from prefetch directory
     prefetch_files = os.listdir(prefetch_directory)
+    
+    # Create file for saving the output
+    with open("PrefetchAnalysis.txt", "w") as file1:
 
-    # Iterate over each file to get full path. 
-    for pf_file in prefetch_files:
-        full_path = prefetch_directory + pf_file
+        # Iterate over each file to get full path. 
+        for pf_file in prefetch_files:
 
-        # Trimming the directory hash from end of the Prefetch filename to get executable name
-        exe_name = pf_file[:-12]
-        print("Executable filename: ", exe_name)
+            full_path = prefetch_directory + pf_file
+        
+            # Trimming the directory hash from end of the Prefetch filename to get executable name
+            exe_name = pf_file[:-12]
 
-        # Retrieving timestamp of file created and convert it into readable format
-        created = os.path.getctime(full_path)
-        created = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(created))
+            file1.write("Executable filename: %s\n" %exe_name)
 
-        # Retrieving timestamp of file modified and convert it into readable format
-        file_modified = os.path.getmtime(full_path)
-        file_modified = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(file_modified))
+            # Retrieving timestamp of file created and convert it into readable format
+            created = os.path.getctime(full_path)
+            created = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(created))
 
-        print("file creation time: ", created)
-        print("file modified time: ", file_modified, "\n")
+            accessed = os.path.getatime(full_path)
+            accessed = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(accessed))
+
+            # Retrieving timestamp of file modified and convert it into readable format
+            file_modified = os.path.getmtime(full_path)
+            file_modified = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(file_modified))
+        
+            file1.write("File modified time: %s\n" %file_modified)
+            file1.write("file accessed time: %s\n" %accessed)
+            file1.write("file creation time: %s\n" %created)
+            file1.write("\n")
 
 if __name__ == "__main__":
     main()
