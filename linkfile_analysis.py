a54a7798656c489591b08d6262281072c51b073c
DreamoryIndia <62346630+DreamoryIndia@users.noreply.github.com>
2020-08-05 14:05:51 -0400

    Update linkfile_analysis.py

--- linkfile_analysis.py
+++ linkfile_analysis.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 import pylnk3
 
+# Create a file for saving the output
+with open("Linkfile_analysis.txt","w") as file1:
+
+    # Retrieving relevant information by parsing link file.
+    link_info = pylnk3.parse("./test.lnk")
+    file1.write("Link File Information:\n %s" %link_info)
