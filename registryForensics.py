efb036fd838778eac5b9a7b1ca09eb37def5d04e
DreamoryIndia <62346630+DreamoryIndia@users.noreply.github.com>
2020-08-05 13:47:51 -0400

    Update registryForensics.py

--- registryForensics.py
+++ registryForensics.py
@@ -11,30 +11,29 @@ import regipy
 reg = regipy.registry.RegistryHive('./123NTUSER.DAT')
 
 # Creating file for saving output
-f = open('RegisterAnalysis.txt', 'a')
+with open('RegistryAnalysis.txt', 'w') as file1:
 
-#General Information About Registry Under Analysis
-f.write("REGISTRY HEADER : %s\n" %reg.header)
-f.write("REGISTRY HIVE TYPE : %s\n" %reg.hive_type)
-f.write("REGISTRY NAME : %s\n" %reg.name)
-f.write("\n")
+    #General Information About Registry Under Analysis
+    file1.write("REGISTRY HEADER : %s\n" %reg.header)
+    file1.write("REGISTRY HIVE TYPE : %s\n" %reg.hive_type)
+    file1.write("REGISTRY NAME : %s\n" %reg.name)
+    file1.write("\n")
 
-#Iterate Recursively Over the Entire Hive from ROOT Key
-for item in reg.recurse_subkeys(as_json=True):
-    f.write("SUBKEYS : %s\n" %item)
-    f.write("\n")
+    #Iterate Recursively Over the Entire Hive from ROOT Key
+    for item in reg.recurse_subkeys(as_json=True):
+        file1.write("SUBKEYS : %s\n" %item)
+        file1.write("\n")
 
-#Iterate Over a Key and Get all Subkeys and Their Modification Time
-for sk in reg.get_key('\Software').iter_subkeys():
-    #print(sk.name, convert_wintime(sk.header.last_modified).isoformat())
-    f.write("Subkey Name : %s\n" %sk.name)
+    #Iterate Over a Key and Get all Subkeys 
+    for sk in reg.get_key('\Software').iter_subkeys():
+        file1.write("Subkey Name : %s\n" %sk.name)
 
-#Get the Values of a Key
-#reg.get_key('Software\Microsoft\Intenet Explorer\BrowserEmulation').get_values(as_json=True)
+    #Get the Values of a Key
+    #reg.get_key('Software\Microsoft\Internet Explorer\BrowserEmulation').get_values(as_json=True)
 
-#Run All Relevant Plugins for a Specific Hive
-#reg = RegistryHive('')
-#run_relevant_plugins(reg, as_json=True)
+    #Run All Relevant Plugins for a Specific Hive
+    #reg = RegistryHive('')
+    #run_relevant_plugins(reg, as_json=True)
 
 
 
