e612cab13531875423605307a00fd85c960f9ff7
DreamoryIndia <62346630+DreamoryIndia@users.noreply.github.com>
2020-08-05 13:52:19 -0400

    Update eventlogs_forensics.py

--- eventlogs_forensics.py
+++ eventlogs_forensics.py
@@ -7,15 +7,18 @@ import Evtx.Views as e_views
 
 def main():
 
-    # Open evtx file using Evtx and convert it into XML.
-    with evtx.Evtx("/home/kajol/LawFact/EventLogAnalysis/logs/System.evtx") as log:
-        print(e_views.XML_HEADER)
-        print("<Events>")
+    # Create a file for saving the output
+    with open("EventlogAnalysis.txt","w") as file1:
+
+        # Open evtx file using Evtx and convert it into XML.
+        with evtx.Evtx("/home/kajol/LawFact/EventLogAnalysis/logs/System.evtx") as log:
+            file1.write(e_views.XML_HEADER)
+            file1.write("<Events>")
         
-        #Get each of the records from this evtx file.
-        for record in log.records():
-            print(record.xml())
-        print("</Events>")
+            # Get each of the records from this evtx file.
+            for record in log.records():
+                file1.write(record.xml())
+            file1.write("</Events>")
 
 if __name__ == "__main__":
     main()
